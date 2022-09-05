1445766e16408bcf98c2bdaa74d2af74d766250b
Samuel Thomas <samuel.thomas93@ntlworld.com>
2022-09-05 12:01:48 +0100

    adding completed exercise files

--- src/token.js
+++ src/token.js
@@ -5,17 +5,26 @@ const jwt = require('jsonwebtoken');
  * 
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#jwtsignpayload-secretorprivatekey-options-callback
  */
-function createToken() {
-    
+function createToken(payload, secretOrPrivateKey) {
+    const token = jwt.sign(payload, secretOrPrivateKey)
+
+    return token
 }
+
+console.log(createToken({    "id": 153,
+"username": "sanchez",
+"email": "rick@sanchez.com",
+"role": "ADMIN"}, '87764d1a-92dc-4ced-a758-9c898c31d525'))
 
 /**
  * Implement this function to accept a payload, secret key and an expiry time, and return a JWT with an expiry
  * 
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#token-expiration-exp-claim
  */
-function createTokenWithExpiry() {
-    
+function createTokenWithExpiry(payload, secretOrPrivateKey, expiry) {
+    const token = jwt.sign(payload, secretOrPrivateKey, {expiresIn: expiry})
+
+    return token
 }
 
 /**
@@ -23,8 +32,13 @@ function createTokenWithExpiry() {
  * 
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
  */
-function verifyToken() {
-
+function verifyToken(jwtToken, secret) {
+     try {
+        const decodedToken = jwt.verify(jwtToken, secret);
+        return decodedToken
+    } catch(err) {
+        return false
+    }
 }
 
 module.exports = {
